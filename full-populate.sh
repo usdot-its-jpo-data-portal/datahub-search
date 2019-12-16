8a9b028b5d48dfba0c4e5477e89b6c3ed453d3f8
mvs5465 <mvs5465@gmail.com>
2019-12-16 12:17:15 -0500

    Add and remove files for CodeBuild Docker image creation

---
+++ full-populate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$ELASTICSEARCH_URL" ]; then
+    echo "Error: Required environment variable ELASTICSEARCH_URL not set."
+    exit 1
+fi
+
+HOST=$ELASTICSEARCH_URL
+
+STATUS=$(curl -so /dev/null -w '%{response_code}' $HOST)
+if [ "$STATUS" -ne 200 ]; then
+  echo 'Error: Connection to ES failed.'
+  exit 1
+fi
+
+echo 'Creating index: DataAssets'
+curl -s -XPUT $HOST/projects/ -H "Content-Type: application/json" -d @/schemas/dataassets-index.json
+echo 'Done creating index: DataAssets'
+
+echo 'Creating index: Related'
+curl -s -XPUT $HOST/code/ -H "Content-Type: application/json" -d @/schemas/related-index.json
+echo 'Done creating index: Related'
+
+echo 'Adding data to index: DataAssets'
+elasticdump --input=/data/dataassets-data.json --output=$ELASTICSEARCH_URL/dataassets --type=data
+echo 'Done adding data to index: DataAssets'
+
+echo 'Adding data to index: Related'
+elasticdump --input=/data/related-data.json --output=$ELASTICSEARCH_URL/related --type=data
+echo 'Done adding data to index: Related'
